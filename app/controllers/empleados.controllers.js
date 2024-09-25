0866e6573391190ffd14d26bde0e6f65f259d047
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-09-25 10:38:12 -0500

    Crear empleado con contraseña encriptada

--- app/controllers/empleados.controllers.js
+++ app/controllers/empleados.controllers.js
@@ -6,15 +6,15 @@ config();
 export const crearEmpleado = async (req, res) => {
     const { identificacion, nombre, apellido, usuario, celular, direccion, email, contrasena, rol } = req.body;
 
-    console.log('Datos recibidos:', req.body); // Para depuración
+    console.log('Datos recibidos:', req.body); 
+
+    const hashedPassword = await bcrypt.hash(contrasena, saltRounds);
 
     try {
-        const [respuesta] = await basedatos.query(`CALL SP_CREAR_EMPLEADO('${rol}','${usuario}','${identificacion}','${nombre}','${apellido}','${celular}','${direccion}','${contrasena}','${email}');`);
+        const [respuesta] = await basedatos.query(`CALL SP_CREAR_EMPLEADO('${rol}','${usuario}','${identificacion}','${nombre}','${apellido}','${celular}','${direccion}','${hashedPassword}','${email}');`);
         console.log('Respuesta de la base de datos:', respuesta); 
     } catch (error) {
         console.error('Error al crear empleado:', error);
         res.status(500).json({ error: 'Error al crear empleado', detalles: error });
     }
 };
-
-    // const hashedPassword = await bcrypt.hash(contrasena, saltRounds);
