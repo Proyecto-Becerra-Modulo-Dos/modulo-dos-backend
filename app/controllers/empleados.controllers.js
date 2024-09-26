4f729218c061e23c6cd461de86e48e305498d713
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-09-26 12:34:52 -0500

    Ultima modificación para agregar empleado

--- app/controllers/empleados.controllers.js
+++ app/controllers/empleados.controllers.js
@@ -7,14 +7,12 @@ const saltRounds = 10;
 
 export const crearEmpleado = async (req, res) => {
     const { identificacion, nombre, apellido, usuario, celular, direccion, email, contrasena, rol } = req.body;
-
-    console.log('Datos recibidos:', req.body); 
-
     const hashedPassword = await bcrypt.hash(contrasena, saltRounds);
 
     try {
         const [respuesta] = await basedatos.query(`CALL SP_CREAR_EMPLEADO('${rol}','${usuario}','${identificacion}','${nombre}','${apellido}','${celular}','${direccion}','${hashedPassword}','${email}');`);
-        console.log('Respuesta de la base de datos:', respuesta); 
+        console.log('Respuesta base de datos:', respuesta); 
+        res.status(201).json({ message: 'Empleado creado exitosamente'});
     } catch (error) {
         console.error('Error al crear empleado:', error);
         res.status(500).json({ error: 'Error al crear empleado', detalles: error });
