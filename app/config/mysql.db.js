3de41971be1ddbe694ad858ee0f384a023a99a8b
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-10-04 09:41:23 -0500

    Método editar plan listo

--- app/config/mysql.db.js
+++ app/config/mysql.db.js
@@ -10,3 +10,11 @@ export const basedatos = createPool({
     port: process.env.MYSQLPORT || 3306,
     database: process.env.MYSQLDATABASE || 'adso',
 })
+
+export const pool = createPool({
+    host: process.env.MYSQL_HOST || 'localhost',
+    user: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || '',
+    port: process.env.MYSQL_PORT || 3306,
+    database: process.env.MYSQL_DATABASE || 'adso',
+})
--- app/controllers/compensaciones.controllers.js
+++ app/controllers/compensaciones.controllers.js
@@ -1,11 +1,33 @@
-import { basedatos } from "../config/mysql.db";
+import dayjs from "dayjs";
+import { pool } from "../config/mysql.db";
+import 'dayjs/locale/es.js';
 import { config } from "dotenv";
 config();
 
+dayjs.locale('es');
+
 export const listarCompensaciones = async (req, res) => {
     try {
-        const [rows] = await basedatos.query("CALL SP_VER_COMPENSACIONES()");
-        res.status(200).json({ compensaciones: rows[0] });
+        const [rows] = await pool.query("CALL SP_VER_COMPENSACIONES()");
+        const compensaciones = rows[0].map(compensacion => {
+            const fechaInicio = new Date(compensacion.fechaInicio).toLocaleDateString('es-ES', {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit'
+            });
+            const fechaFin = new Date(compensacion.fechaFin).toLocaleDateString('es-ES', {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit'
+            });
+            return {
+                ...compensacion,
+                fechaInicio,
+                fechaFin
+            };
+        })
+        console.log(compensaciones);
+        res.status(200).json({ compensaciones });
     } catch (error) {
         console.log(error);
         
@@ -14,10 +36,10 @@ export const listarCompensaciones = async (req, res) => {
 };
 
 export const crearCompensacion = async (req, res) => {
-    const { cedula, salario, bonificaciones, incentivos, tiempoLibre } = req.body;
+    const { cedula, salario, bonificaciones, incentivos, fechaInicio, fechaFin } = req.body;
 
     try {
-        const [respuesta] = await basedatos.query(`CALL SP_CREAR_COMPENSACION('${tiempoLibre}','${salario}','${cedula}','${bonificaciones}','${incentivos}');`);
+        const [respuesta] = await pool.query(`CALL SP_CREAR_COMPENSACION('${fechaInicio}', '${fechaFin}','${salario}','${cedula}','${bonificaciones}','${incentivos}');`);
         console.log('Respuesta base de datos:', respuesta); 
         res.status(201).json({ message: 'Plan creado exitosamente'});
     } catch (error) {
@@ -30,9 +52,18 @@ export const obtenerCompensacion = async (req, res) => {
     const id = req.params.id;
 
     try {
-        const respuesta = await basedatos.query(`CALL LL_OBTENER_COMPENSACION('${id}');`);
+        const respuesta = await pool.query(`CALL SP_OBTENER_COMPENSACION('${id}');`);
+        const compensaciones = respuesta[0][0].map(compensacion => {
+            const fechaInicio = new Date(compensacion.fechaInicio).toISOString().split('T')[0]; // Formato yyyy-MM-dd
+            const fechaFin = new Date(compensacion.fechaFin).toISOString().split('T')[0]; // Formato yyyy-MM-dd
+            return {
+                ...compensacion,
+                fechaInicio,
+                fechaFin
+            };
+        });
         if (respuesta.length > 0) {
-            res.status(200).json(respuesta[0]);
+            res.status(200).json(compensaciones[0]);
         } else {
             res.status(404).json({ mensaje: "Plan no encontrado" });
         }
@@ -43,10 +74,10 @@ export const obtenerCompensacion = async (req, res) => {
 };
 
 export const editarCompensacion = async (req, res) => {
-    const { salario, bonificaciones, incentivos, tiempoLibre, id } = req.body;
+    const { salario, bonificacion, incentivo, fechaInicio, fechaFin, id } = req.body;
 
     try {
-        const respuesta = await basedatos.query(`CALL LL_EDITAR_COMPENSACION('${salario}', '${bonificaciones}','${incentivos}', '${tiempoLibre}', '${id}');`);
+        const respuesta = await pool.query(`CALL SP_EDITAR_COMPENSACION('${salario}', '${incentivo}', '${bonificacion}', '${fechaInicio}', '${fechaFin}', '${id}');`);
         res.status(200).json({ mensaje: 'Plan editado exitosamente' });
     } catch (error) {
         console.error(error);
@@ -58,7 +89,7 @@ export const desactivarCompensacion = async (req, res) => {
     const id = req.body.id;
 
     try {
-        const [respuesta] = await basedatos.query(`CALL LL_DESACTIVAR_COMPENSACION('${id}')`);
+        const [respuesta] = await pool.query(`CALL SP_DESACTIVAR_COMPENSACION('${id}')`);
         res.json(respuesta);
     } catch (error) {
         res.status(500).json(error);
