2d64bb862939c695fb26c2f95699a07e1b2bab49
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-10-11 07:28:56 -0500

    Primer intento cargar docs

--- app/app.js
+++ app/app.js
@@ -4,18 +4,34 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import ruta from "./routes/index.js";
+import multer from "multer";
+import path from "path"; 
 dotenv.config();
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, 'uploads')); // Ruta donde se guardarán los archivos
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)); // Renombra el archivo para evitar duplicados
+    }
+});
+
+const upload = multer({ storage });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.json({ file: req.file.filename, message: 'Archivo subido con éxito!' });
+});
 
 app.use("/", ruta);
-app.use(cookieParser())
 
 app.set("port", process.env.PORT || 6000);
 
