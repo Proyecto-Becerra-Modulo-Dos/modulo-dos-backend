af7c758814e6e109dae4ef9cc4cad135261aee5c
Santiago <santipahur@gmail.com>
2024-10-15 18:09:02 -0500

    creacion del backend de trabajo remoto

--- app/controllers/supervisor.controllers.js
+++ app/controllers/supervisor.controllers.js
@@ -2,30 +2,7 @@ import { config } from "dotenv";
 import { basedatos, pool } from "../config/mysql.db";
 config();
 
-export const aprobarPermiso = async (req, res) => {
-  const id = req.body.id;
-
-  try {
-    const [respuesta] = await pool.query(`CALL SP_APROBAR_PERMISO(${id})`);
-    res.json(respuesta);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
-  }
-};
-
-export const rechazarPermiso = async (req, res) => {
-  const id = req.body.id;
-
-  try {
-    const [respuesta] = await pool.query(`CALL SP_RECHAZAR_PERMISO('${id}')`);
-    res.json(respuesta);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
-  }
-};
-
+// Permiso Ausencia
 export const verPermisos = async (req, res) => {
   try {
     const [datosDB1] = await basedatos.query(`call sp_ver_empleados();`);
@@ -54,6 +31,30 @@ export const verPermisos = async (req, res) => {
     }
 
     res.json(resultados);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+export const aprobarPermiso = async (req, res) => {
+  const id = req.body.id;
+
+  try {
+    const [respuesta] = await pool.query(`call sp_aprobar_permiso(${id})`);
+    res.json(respuesta);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+export const rechazarPermiso = async (req, res) => {
+  const id = req.body.id;
+
+  try {
+    const [respuesta] = await pool.query(`call sp_rechazar_permiso('${id}')`);
+    res.json(respuesta);
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
@@ -122,6 +123,55 @@ export const verPermisosRechazados = async (req, res) => {
     }
 
     res.json(resultados);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+// Trabajo Remoto
+export const verTrabajoRemoto = async (req, res) => {
+  try {
+    const [datosDB1] = await basedatos.query(`call sp_ver_empleados();`);
+
+    const [datosDB2] = await pool.query(`call sp_ver_trabajo_remoto();`);
+
+    const resultados = [];
+
+    for (let i = 0; i < datosDB1[0].length; i++) {
+      for (let j = 0; j < datosDB2[0].length; j++) {
+        if (
+          datosDB1[0][i].id === datosDB2[0][j].empleadoid &&
+          datosDB2[0][j].estado === "En proceso"
+        ) {
+          resultados.push({
+            idSoli: datosDB2[0][j].idSolicitudes,
+            nombre: datosDB1[0][i].nombre,
+            apellido: datosDB1[0][i].apellido,
+            email: datosDB1[0][i].email,
+            descripcion: datosDB2[0][j].descripcion,
+            rol: datosDB1[0][i].rol,
+            estado: datosDB2[0][j].estado,
+          });
+        }
+      }
+    }
+
+    res.json(resultados);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+export const aprobarTrabajoRemoto = async (req, res) => {
+  const id = req.body.id;
+
+  try {
+    const [respuesta] = await pool.query(
+      `call sp_aprobar_trabajo_remoto(${id})`
+    );
+    res.json(respuesta);
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
--- app/routes/supervisor.routes.js
+++ app/routes/supervisor.routes.js
@@ -1,18 +1,23 @@
 import { Router } from "express";
 import {
   aprobarPermiso,
+  aprobarTrabajoRemoto,
   rechazarPermiso,
   verPermisos,
   verPermisosAprobados,
   verPermisosRechazados,
+  verTrabajoRemoto,
 } from "../controllers/supervisor.controllers";
 
 const rutaSupervisor = Router();
 
+rutaSupervisor.get("/verPermisos", verPermisos);
 rutaSupervisor.post("/rechazarPermiso", rechazarPermiso);
 rutaSupervisor.post("/aprobarPermiso", aprobarPermiso);
-rutaSupervisor.get("/verPermisos", verPermisos);
 rutaSupervisor.get("/verPermisosAprobados", verPermisosAprobados);
 rutaSupervisor.get("/verPermisosRechazados", verPermisosRechazados);
+
+rutaSupervisor.get("/verTrabajoRemoto", verTrabajoRemoto);
+rutaSupervisor.post("/aprobarTrabajoRemoto", aprobarTrabajoRemoto);
 
 export default rutaSupervisor;
