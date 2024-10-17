f42540016a3b1725332e16bce359a3ebca02030c
Caro-Villa005 <ccastrillon614@gmail.com>
2024-10-17 10:38:21 -0500

    Se realizo el funcionamiento de la vista objetivos

--- app/controllers/empleados.controllers.js
+++ app/controllers/empleados.controllers.js
@@ -5,6 +5,73 @@ config();
 
 const saltRounds = 10;
 
+//Ver objetivo
+export const verObjetivos = async(req, res) => {
+    try {
+        const [rows] = await basedatos.query("CALL SP_LISTAR_OBJETIVOS()");
+        res.status(200).json({ message: rows[0] });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+//Crear objetivo
+export const crearObjetivo = async (req, res) => {
+    const { objetivo, empleadoid, estado } = req.body;
+
+    try {
+        const [respuesta] = await basedatos.query(`CALL SP_CREAR_OBJETIVO('${objetivo}','${empleadoid}','${estado}');`);
+        console.log('Respuesta base de datos:', respuesta);
+        res.status(201).json({ message: 'Objetivo creado exitosamente' });
+    } catch (error) {
+        console.error('Error al crear objetivo:', error);
+        res.status(500).json({ error: 'Error al crear objetivo', detalles: error });
+    }
+};
+
+
+//Eliminar objetivo
+export const eliminarObjetivo = async (req, res) => {
+    const { idDesarrollo_Profesional } = req.body;
+    console.log(idDesarrollo_Profesional);
+    
+
+    try {
+        const [respuesta] = await basedatos.query(`CALL SP_ELIMINAR_OBJETIVOS('${idDesarrollo_Profesional}');`);
+        console.log('Respuesta base de datos:', respuesta);
+        res.status(201).json({ message: 'Objetivo eliminado exitosamente' });
+    } catch (error) {
+        console.error('Error al crear objetivo:', error);
+        res.status(500).json({ error: 'Error al eliminar objetivo', detalles: error });
+    }
+};
+
+//Editar objetivo
+export const editarObjetivo = (req, res) => {
+    const { idDesarrollo_Profesional, objetivo} = req.body;
+    try {
+        const request = basedatos.query("CALL SP_EDITAR_OBJETIVOS(?, ?)", [idDesarrollo_Profesional, objetivo])
+        res.status(201).json({ message: 'Objetivos actualizados' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error al editar objetivo', detalles: err });
+    }
+};
+
+//msotrar objetivo
+export const mostrarObjetivo = async (req, res) => {
+    const { idDesarrollo_Profesional } = req.body;
+
+    try {
+        const [respuesta] = await basedatos.query(`CALL SP_MOSTRAR_OBJETIVO('${idDesarrollo_Profesional}');`);
+        console.log('Respuesta base de datos:', respuesta);
+        res.status(200).json({ message: respuesta[0] });
+    } catch (error) {
+        console.error('Error al crear objetivo:', error);
+        res.status(500).json({ error: 'Error al crear objetivo', detalles: error });
+    }
+};
+
 export const listarEmpleados = async (req, res) => {
     try {
         const [rows] = await basedatos.query("CALL SP_VER_EMPLEADOS()");
@@ -26,7 +93,6 @@ export const crearEmpleado = async (req, res) => {
         console.error('Error al crear empleado:', error);
         res.status(500).json({ error: 'Error al crear empleado', detalles: error });
     }
-
 };
 
 export const verNomina = async (req, res) => {
--- app/routes/empleados.routes.js
+++ app/routes/empleados.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { crearEmpleado, listarEmpleados, verNomina } from "../controllers/empleados.controllers";
+import { crearEmpleado, crearObjetivo, editarObjetivo, eliminarObjetivo, listarEmpleados, mostrarObjetivo, verNomina, verObjetivos } from "../controllers/empleados.controllers";
 
 const rutaEmpleados = Router();
 
 rutaEmpleados.get("/", listarEmpleados);
 rutaEmpleados.post("/crear", crearEmpleado);
 rutaEmpleados.post("/vernomina", verNomina);
+rutaEmpleados.get("/verObjetivos", verObjetivos);
+rutaEmpleados.post("/crearObjetivo", crearObjetivo);
+rutaEmpleados.delete("/eliminarObjetivo", eliminarObjetivo);
+rutaEmpleados.put("/editarObjetivo", editarObjetivo);
+rutaEmpleados.post("/mostrarObjetivo", mostrarObjetivo);
 
 export default rutaEmpleados;
